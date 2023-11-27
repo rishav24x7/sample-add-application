6304cad9d39c2e1f24125c1c4f0e3041d01371e0
Rishav Srivastava <RishavSrivastava@fico.com>
2023-11-27 21:24:46 +0530

    added sonar and jacoco

--- src/main/java/org/rishav/Main.java
+++ src/main/java/org/rishav/Main.java
@@ -2,7 +2,7 @@ package org.rishav;
 
 public class Main {
 
-    public int add_numbers(int a, int b){
+    public int addNumbers(int a, int b){
         return a+b;
     }
 }
--- src/test/java/org/rishav/MainTest.java
+++ src/test/java/org/rishav/MainTest.java
@@ -8,10 +8,10 @@ import static org.junit.jupiter.api.Assertions.*;
 public class MainTest {
 
     @Test
-    public void add_number_test(){
+    public void addNumber_test(){
         Main main_object = new Main();
-        int result = main_object.add_numbers(2,3);
-        Assertions.assertEquals(result,5);
+        int result = main_object.addNumbers(2,3);
+        Assertions.assertEquals(5, result);
     }
 
 }
