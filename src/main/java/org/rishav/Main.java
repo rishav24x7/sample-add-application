17c60b2b6d81d5853aa2cd25dace64cbfa08b497
Rishav Srivastava <RishavSrivastava@fico.com>
2023-11-28 18:36:36 +0530

    added sonarqube workflow and new test cases

--- src/main/java/org/rishav/Main.java
+++ src/main/java/org/rishav/Main.java
@@ -5,4 +5,15 @@ public class Main {
     public int addNumbers(int a, int b){
         return a+b;
     }
+
+    public int subtractNumbers(int a, int b){
+        return a-b;
+    }
+
+    public boolean compareNumbers(int a, int b){
+        if (a>b)
+            return true;
+        else
+            return false;
+    }
 }
